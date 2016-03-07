854b9b6842f19a44c3df3f91bef91f2a6bee4c06
Ching Wong <ching.wong15@imperial.ac.uk>
2016-03-07 12:50:22 +0000

    7_3_16 main

--- src/dictionary/Main.java
+++ src/dictionary/Main.java
@@ -1,9 +1,8 @@
 package dictionary;
 
-import com.sun.org.apache.regexp.internal.RE;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+
+import java.io.*;
 import java.util.Random;
 
 public class Main {
@@ -13,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+
       Random random = new Random();
 
       InsertComplexities complexities = new InsertComplexities(random);
@@ -21,12 +21,29 @@ public class Main {
       int[] listComplex = complexities.getInsertComplexities(list, MAX_SIZE, REPITITIONS);
 
       Dictionary<InsertComplexities.InstrumentedKey, Integer> tree = new BinarySearchTree<>();
-      int[] treeCompex = complexities.getInsertComplexities(tree, MAX_SIZE, REPITITIONS);
+      int[] treeComplex = complexities.getInsertComplexities(tree, MAX_SIZE, REPITITIONS);
 
-      File treeFile = new File("BinarySearchTree.dat");
-      File listFile = new File(" OrderedLinkedList.dat");
+
+
+      PrintWriter listFile = new PrintWriter("OrderedLinkedList.dat");
+      //BufferedWriter listWriter = new BufferedWriter(listFile);
+
+      PrintWriter treeFile = new PrintWriter("BinarySearchTree.dat");
+      //BufferedWriter treeWriter = new BufferedWriter(treeFile);
+
+      //File treeFile = new File("BinarySearchTree.dat");
+      //File listFile = new File(" OrderedLinkedList.dat");
       //FileWriter treeWriter = new FileWriter("BinarySearchTree.dat");
 
+      for (int i = 0; i < MAX_SIZE; i++) {
+        listFile.println(i + "\t" + listComplex[i]);
+        treeFile.println(i + "\t" + treeComplex[i]);
+      }
+
+      listFile.flush();
+      treeFile.flush();
+      listFile.close();
+      treeFile.close();
     }
 
 }
