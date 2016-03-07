fea696d11f655adca74d02ec07cb3a2aa4f331e9
Ching Wong <ching.wong15@imperial.ac.uk>
2016-03-07 01:57:48 +0000

    6_3_16 02:00 doing main

--- src/dictionary/Main.java
+++ src/dictionary/Main.java
@@ -1,6 +1,10 @@
 package dictionary;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 
 public class Main {
 
@@ -8,7 +12,21 @@ public class Main {
     private static final int REPITITIONS = 500;
 
     public static void main(String[] args) throws FileNotFoundException {
-        // TODO
+
+      Random random = new Random();
+
+      InsertComplexities complexities = new InsertComplexities(random);
+
+      Dictionary<InsertComplexities.InstrumentedKey, Integer> list = new OrderedLinkedList<>();
+      int[] listComplex = complexities.getInsertComplexities(list, MAX_SIZE, REPITITIONS);
+
+      Dictionary<InsertComplexities.InstrumentedKey, Integer> tree = new BinarySearchTree<>();
+      int[] treeCompex = complexities.getInsertComplexities(tree, MAX_SIZE, REPITITIONS);
+
+      File treeFile = new File("BinarySearchTree.dat");
+      File listFile = new File(" OrderedLinkedList.dat");
+      //FileWriter treeWriter = new FileWriter("BinarySearchTree.dat");
+
     }
 
 }
